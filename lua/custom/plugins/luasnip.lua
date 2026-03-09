c3e33348fc905d69252d86aa86b442c4b6a65c4c
spcameron <cameron.seanpatrick@gmail.com>
2026-03-09 08:35:33 -0400

    Attempt to fix tab jump issue

--- lua/custom/plugins/luasnip.lua
+++ lua/custom/plugins/luasnip.lua
@@ -13,6 +13,30 @@ return {
     { 'rafamadriz/friendly-snippets' },
   },
   opts = {},
+  config = function(_, opts)
+    local luasnip = require 'luasnip'
+
+    luasnip.setup(opts)
+
+    local group = vim.api.nvim_create_augroup('spc_luasnip_cleanup', { clear = true })
+
+    local function leave_snippet()
+      if
+        ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and luasnip.session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not luasnip.session.jump_active
+      then
+        luasnip.unlink_current()
+      end
+    end
+
+    vim.api.nvim_create_autocmd('ModeChanged', {
+      group = group,
+      pattern = '*',
+      callback = leave_snippet,
+      desc = 'Unlink LuaSnip snippet when leaving insert/select mode',
+    })
+  end,
 }
 
 -- vim: ts=2 sts=2 sw=2 et
